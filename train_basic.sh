95ead363d059ff8f1bee07e4e10e44ace271a1d6
luisCossioUOH <luis.cossio@uoh.cl>
2025-06-24 12:26:34 -0400

    updated file for training

--- train_basic.sh
+++ train_basic.sh
@@ -5,16 +5,16 @@
 #SBATCH --output=.logs/out_train.out
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --time=60:0
-#SBATCH --mem-per-cpu=5G
+#SBATCH --time=02:30:0
+#SBATCH --mem-per-cpu=25G
 #SBATCH --nodelist=v100
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-task=1
 #SBATCH --mail-type=END
 #SBATCH --mail-user=luis.cossio@uoh.cl
 
-
 cd ../sifs
-srun singularity instance start --nv ultra.sif ultra1
+singularity instance start --nv ultra.sif ultra1
 cd ../ultra
-srun singularity exec  --nv instance://ultra1 python train_cherry_co.py --epoch>
+singularity exec  --nv instance://ultra1 python train_ultralytics.py --dataset-config conf>
+singularity instance stop ultra1
