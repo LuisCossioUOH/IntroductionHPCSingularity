61df1846c1afed3e0db9e078a33290de8ca33b12
luisCossioUOH <luis.cossio@uoh.cl>
2025-06-23 20:38:54 -0400

    presentation added

--- train_basic.sh
+++ train_basic.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SBATCH --job-name=train_basic_ultra
-SBATCH --error=/mnt/beegfs/home/lcossio/scripts/.logs/res.err
-SBATCH --output=/mnt/beegfs/home/lcossio/scripts/.logs/res.out
-SBATCH --cpus-per-task=1
-SBATCH --ntask=1
-SBATCH --time=6:30:0
-SBATCH --mem-per-cpu=9G
-SBATCH --nodelist=v100
-SBATCH --gres=gpu:1
-SBATCH --gpus-per-task=1
-SBATCH --mailtype=ALL
-SBATCH --mail-list=luis.cossio@uoh.cl
+#SBATCH --job-name=train_basic
+#SBATCH --error=.logs/res_train.err
+#SBATCH --output=.logs/out_train.out
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --time=60:0
+#SBATCH --mem-per-cpu=5G
+#SBATCH --nodelist=v100
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=luis.cossio@uoh.cl
+
 
 cd ../sifs
-singularity instance start ultra.sif ultra1
+srun singularity instance start --nv ultra.sif ultra1
 cd ../ultra
-singularity exec instance://ultra1 python train_cherry_co.py --epochs 50
-singularity instance stop ultra1
+srun singularity exec  --nv instance://ultra1 python train_cherry_co.py --epoch>
